e27079a3c9da8fa42320cc0f53181d921e62d4da
natan <mrnatande@gmail.com>
2017-08-21 00:16:19 +0800

    pattern

--- pattern.go
+++ pattern.go
@@ -6,6 +6,22 @@ import (
 	"time"
 )
 
+const (
+	RegexpVersion     = "^[1-9]\\d*(\\.\\d*)*([\\w-]+)?$"
+	RegexpMobile_ZHCN = "^1[34578][0-9]{9}$"
+	RegexpEmail       = "^[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{2,3})$"
+	RegexpVariable    = "^[_a-zA-Z][_a-zA-Z0-9]*$"
+)
+
+const (
+	PatternVersion   = "version"
+	PatternURL       = "url"
+	PatternEmail     = "email"
+	PatternVariable  = "variable"
+	PatternMobile    = "mobile"
+	PatternBirthDate = "birth_date"
+)
+
 type PatternMatcher interface {
 	Match(i interface{}) bool
 }
@@ -28,29 +44,13 @@ func (r *Regexp) Match(i interface{}) bool {
 }
 
 var _patternToMatcher = map[string]PatternMatcher{
-	"version":   (*Regexp)(regexp.MustCompile(RegexpVersion)),
-	"mobile":    (*Regexp)(regexp.MustCompile(RegexpMobile_ZHCN)),
-	"email":     (*Regexp)(regexp.MustCompile(RegexpEmail)),
-	"variable":  (*Regexp)(regexp.MustCompile(RegexpVariable)),
-	"birthdate": PatternMatchFunc(MatchBirthDate),
-	"url":       PatternMatchFunc(MatchURL),
+	PatternVersion:   (*Regexp)(regexp.MustCompile(RegexpVersion)),
+	PatternMobile:    (*Regexp)(regexp.MustCompile(RegexpMobile_ZHCN)),
+	PatternEmail:     (*Regexp)(regexp.MustCompile(RegexpEmail)),
+	PatternVariable:  (*Regexp)(regexp.MustCompile(RegexpVariable)),
+	PatternBirthDate: PatternMatchFunc(MatchBirthDate),
+	PatternURL:       PatternMatchFunc(MatchURL),
 }
-
-const (
-	RegexpVersion     = "^[1-9]\\d*(\\.\\d*)*([\\w-]+)?$"
-	RegexpMobile_ZHCN = "^1[34578][0-9]{9}$"
-	RegexpEmail       = "^[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{2,3})$"
-	RegexpVariable    = "^[_a-zA-Z][_a-zA-Z0-9]*$"
-)
-
-const (
-	PatternVersion   = "version"
-	PatternURL       = "url"
-	PatternEmail     = "email"
-	PatternVariable  = "variable"
-	PatternMobile    = "mobile"
-	PatternBirthDate = "birthdate"
-)
 
 func MatchPattern(name string, v interface{}) bool {
 	return _patternToMatcher[name].Match(v)
