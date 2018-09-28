0da40ed0af88dc9a8db791c1233b9a864a585c54
natan <mrnatande@gmail.com>
2018-09-28 10:19:24 +0800

    Email pattern

--- pattern.go
+++ pattern.go
@@ -9,7 +9,7 @@ import (
 const (
 	RegexpVersion     = "^[1-9]\\d*(\\.\\d*)*([\\w-]+)?$"
 	RegexpMobile_ZHCN = "^1[34578][0-9]{9}$"
-	RegexpEmail       = "^[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{2,3})$"
+	RegexpEmail       = "^[_a-zA-Z0-9-\\+\\.]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{2,3})$"
 	RegexpVariable    = "^[_a-zA-Z][_a-zA-Z0-9]*$"
 )
 
